17c94ab74251106232a044a69ffb72b7cf8d1263
2020-DEV-035 <2020-DEV-035@2020-DEV-035.com>
2020-06-16 15:18:54 +0200

    Initial Commit

---
+++ TicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TicTacToe
+//
+//  Created by 2020-DEV-035 on 16/06/2020.
+//  Copyright © 2020 2020-DEV-035. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
